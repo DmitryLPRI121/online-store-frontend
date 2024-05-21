8cd28e20d3b58f6a1941eaf9e1d9c783613665f7
DmitryLPRI121 <lom30031974@gmail.com>
2024-05-21 23:53:54 +0300

    Design of the NotFoundPage has been completed.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import "./NotFoundPage.scss"
 import { Navbar } from "../components/Navbar";
+import { Link } from "react-router-dom";
+import image404 from '../images/404.svg';
 
 const NotFoundPage = () => {
     return (
@@ -11,7 +13,14 @@ const NotFoundPage = () => {
             </Helmet>
             <Navbar />
             <main className="NOT_FOUND_PAGE">
-                <p>Not found</p>
+                <div className="NotFoundPageInfo">
+                    <img src={image404} alt="404" />
+                    <h1>Страница не найдена</h1>
+                    <p>Хотите вернуться на главную страницу?</p>
+                    <Link to="/Home">
+                        <button>Вернуться</button>
+                    </Link>
+                </div>
             </main>
         </>
     )
